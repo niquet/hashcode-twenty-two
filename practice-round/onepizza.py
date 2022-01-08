651a001ff335af2d368914627324fdf66ceb21bf
daBoehmer <david.bhmr@gmail.com>
2022-01-08 10:56:17 +0100

    Fixed error when getting input; corrected json dumbs

--- practice-round/onepizza.py
+++ practice-round/onepizza.py
@@ -6,17 +6,15 @@ if __name__ == '__main__':
     clients = {}
     ingredients = set()
 
-    for client in range(2 * C):
+    for client in range(C):
         L = input().strip() # integer 1 <= L <= 5, followed by L ingredients a client likes
         D = input().strip() # integer 0 <= D <= 5, followed by D ingredients a client dislikes
         print(L)
         print(D)
-        #clients[client] = [L[1:], D[1:]]
-        #preferences = L[1:] + D[1:]
-        #ingredients.update(preferences)
-    
-    parsed = json.loads(clients)
-    print(json.dumps(parsed, indent=4, sort_keys=False))
+        clients[client] = [L[2:], D[2:]]
+        preferences = L[2:].split() + D[2:].split()
+        ingredients.update(preferences)
 
-    parsed = json.loads(ingredients)
-    print(json.dumps(parsed, indent=4, sort_keys=False))
+    print(json.dumps(clients, indent=4, sort_keys=False))
+
+    print(json.dumps(list(ingredients), indent=4, sort_keys=False))
